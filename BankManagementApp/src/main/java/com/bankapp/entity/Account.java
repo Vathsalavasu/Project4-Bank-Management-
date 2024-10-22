fdaebc18c0b525f9fb2f8907a47e03aa5f0fa32a
Vathsalavasu <write2vathsalar@gmail.com>
2024-10-22 17:10:39 +0530

    fifth project

---
+++ BankManagementApp/src/main/java/com/bankapp/entity/Account.java
@@ -0,0 +1,58 @@
+package com.bankapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Account {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long account_name;
+	
+	@Column
+	private String account_holder_name;
+	@Column
+	private Double account_balance;
+	
+	public Account() {
+		
+	}
+	
+	public Account(String account_holder_name, Double account_balance) {
+		super();
+		this.account_holder_name = account_holder_name;
+		this.account_balance = account_balance;
+	}
+
+	public Long getAccount_name() {
+		return account_name;
+	}
+	public void setAccount_name(Long account_name) {
+		this.account_name = account_name;
+	}
+	public String getAccount_holder_name() {
+		return account_holder_name;
+	}
+	public void setAccount_holder_name(String account_holder_name) {
+		this.account_holder_name = account_holder_name;
+	}
+	public Double getAccount_balance() {
+		return account_balance;
+	}
+	public void setAccount_balance(Double account_balance) {
+		this.account_balance = account_balance;
+	}
+	@Override
+	public String toString() {
+		return "Account [account_name=" + account_name + ", account_holder_name=" + account_holder_name
+				+ ", account_balance=" + account_balance + "]";
+	}
+	
+
+}
